745664a957fa4cd34cc90285042bc2f2278eb7d4
Bastián Poloni <128436347+bastianpoloni@users.noreply.github.com>
2023-07-18 22:36:37 -0400

    Primer commit

---
+++ app.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import {v4 as uuidv4} from 'uuid';
+import axios from 'axios';
+import fs  from 'fs';
+import moment from 'moment';
+import _ from 'lodash';
+import chalk from 'chalk';
+
+
+const app = express();
+const path = "./files/citas.json";
+let citas = JSON.parse(fs.readFileSync(path));
+let id = uuidv4();
+const log = console.log;
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    citas = JSON.parse(fs.readFileSync(path));
+    res.send(citas);
+    log(chalk.blue.bgWhite(citas));
+    
+})
+
+app.post('/', (req, res) => {
+   addUser();
+   res.send("Usuario agregado");
+})
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
+});
+
+
+async function getData(){
+    try{
+        const response = await axios.get('https://randomuser.me/api/');
+        let user = response.data;
+        return user;
+    }catch(error){
+        console.log(error);
+    }
+}
+
+async function addUser(){
+    try{
+        const user = await getData();
+        citas.push({
+            nombre: user.results[0].name.first,
+            apellido: user.results[0].name.last,
+            id : id,
+            timeStamp: moment().format('MMMM DD-YYYY HH:mm:ss'),
+        });
+        fs.writeFileSync(path, JSON.stringify(citas));
+       
+    }catch (error){
+        console.log(error);
+    }
+}
